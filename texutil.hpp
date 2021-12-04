9248c7fdddf2e5721dca98c60734821bf5f3efac
Brennan Shacklett <bpshacklett@gmail.com>
2021-12-03 20:35:55 -0800

    Fix header

--- texutil.hpp
+++ texutil.hpp
@@ -9,7 +9,6 @@ enum class TextureType : uint32_t {
 };
 
 void generateMips(const char *out_path, TextureType tex_type,
-                  const uint8_t *data, uint64_t num_bytes,
-                  void *cb_data);
+                  const uint8_t *data, uint64_t num_bytes);
 
 }
